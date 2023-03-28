b1833d28ee269784013962271f0179c810c3b15a
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-03-28 16:51:10 -0300

    requisito 12

--- desafio9.sql
+++ desafio9.sql
@@ -1,2 +1,2 @@
-SELECT * FROM purchase_orders WHERE notes IS NOT NULL;
+SELECT notes FROM purchase_orders WHERE notes IS NOT NULL;
 -- https://stackoverflow.com/questions/5285448/mysql-select-only-not-null-values
