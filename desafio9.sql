f6062b12fc2de3a64f9ea7b826101016b1f6c0b6
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-03-28 16:43:02 -0300

    formatação

--- desafio9.sql
+++ desafio9.sql
@@ -1,3 +1,2 @@
-SELECT * FROM purchase_orders
-WHERE notes IS NOT NULL;
+SELECT * FROM purchase_orders WHERE notes IS NOT NULL;
 -- https://stackoverflow.com/questions/5285448/mysql-select-only-not-null-values
